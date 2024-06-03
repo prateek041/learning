4bf2e6b3d7c66ed5cdde76d68f233c4ba99c960a
Prateek Singh <prateeksingh9741@gmail.com>
2024-06-03 16:03:23 +0530

    learning vue: passing props

--- vue/expense-tracker/src/components/History.vue
+++ vue/expense-tracker/src/components/History.vue
@@ -1,10 +1,18 @@
 <script setup>
 import Transaction from "./Transaction.vue";
+import { ref } from "vue";
+
+const items = [
+  { item: "hello", price: "world" },
+  { item: "goodbye", price: "world" },
+];
 </script>
 
 <template>
   <div>
     <div>History</div>
-    <Transaction />
+    <li v-for="item in items">
+      <Transaction :item="item.item" :price="item.price" />
+    </li>
   </div>
 </template>
--- vue/expense-tracker/src/components/Transaction.vue
+++ vue/expense-tracker/src/components/Transaction.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 // here we will learn about importing and exporting props into component
+
+const props = defineProps({
+  ['item', 'price']
+});
+
+console.log(prope)
 </script>
 
 <template>
   <div>
-    <p>Item</p>
-    <p>Price</p>
+    <p>{{ item }}</p>
+    <p>{{ price }}</p>
   </div>
 </template>
